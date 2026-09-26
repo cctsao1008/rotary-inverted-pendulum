205019a8c8946876f64fea9ab624e3a28df4c58e
Cheng <chiacheng.tsao@gmail.com>
2026-09-26 08:51:23 +0800

    Add host user LED control helper

--- tools/rp2350_commission/device.py
+++ tools/rp2350_commission/device.py
@@ -156,3 +156,7 @@ class Rp2350Device:
             value0=value,
             duration_ms=lease_ms,
         ).value0
+
+    def set_user_led(self, on: bool) -> bool:
+        ack = self.command(HidCommand.SET_USER_LED, value0=1.0 if on else 0.0)
+        return bool(round(ack.value0))
